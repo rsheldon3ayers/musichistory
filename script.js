7361986a442e94481f7e41e5bc5fbdc64bf5773e
Robert (Bobby) Ayers <rsheldon3ayers@gmail.com>
2015-10-23 08:54:28 -0500

    edited the .css

--- script.js
+++ script.js
@@ -1,51 +1,66 @@
-
+$(document).ready(function() {
 	
-	var addButton = document.getElementById("add-button");
-addButton.addEventListener("click", function(event) {
+	var addButton = $("#add-button").click(function (){
+	console.log();
 	
 	populateList();
+	populateArtistSelect();
+	$("#song-title").val(" ");
+	$("#artist-n").val(" ");
+	$("#album-n").val(" ");
 
 
-	console.log("works");
+	
 });
-var rightBox = document.getElementById("right-box");
-function populateList() {
 
-	var songTitle = document.getElementById("song-title").value;
-	var artistName = document.getElementById("artist-n").value;
-	var albumName = document.getElementById("album-n").value;
+function populateArtistSelect () {
+	console.log("Hi BOB")
+	var  artistOption= $("#artist-name");
+    var artistN = $("#artist-n").val();
+    console.log(artistN);
+    var option = artistOption.append("<option></option>");
+    // artistN.appendTo(option);
+    
+   };
+
+var rightBox = $("#right-box");
+ function populateList() {
+ 	console.log("Hi BOB");
+	var songTitle = $("#song-title").val();
+	var artistName = $("#artist-n").val();
+	console.log(artistName);
+	var albumName = $("#album-n").val();
 	console.log(songTitle, artistName, albumName);
-	var newSong= songTitle + "-" + artistName + "-" + albumName;
+	var newSong= songTitle + " " + artistName + " " + albumName;
 
 	//add new song to the Song List
-	var newList = document.createElement("ul");
+	var newList = "<div>";
 	console.log(newList);
-	newList.innerHTML = newSong;
-	rightBox.insertBefore(newList, rightBox.childNodes[0]);
-
-	document.getElementById("song-title").value = " ";
-	document.getElementById("artist-n").value = " ";
-	document.getElementById("album-n").value = " ";
+	newList += newSong + "</div>";
+	
+	rightBox.prepend(newList);
+	console.log(newSong);
+	
+ //     songTitle.val(" ");
+	// // artistName.val("");
+	// // albumName.html("");
 	
 	console.log(songTitle);
 
 }
-var addClick = document.getElementById("add-click");
-addClick.addEventListener("click", function(event) {
+var addClick = $("#add-click").click(function() {
 	console.log(addClick);
-	document.getElementById("right-box").classList.remove("right-box");
-	document.getElementById("right-box").classList.add("hide-box");
-	document.getElementById("hide-music").classList.remove("hide-box");
-	document.getElementById("hide-music").classList.add("right-box");
-});
-var listMusic = document.getElementById("list-music");
-listMusic.addEventListener("click", function(event) {
-	document.getElementById("right-box").classList.remove("hide-box");
-	document.getElementById("right-box").classList.add("right-box");
-	document.getElementById("hide-music").classList.remove("right-box");
-	document.getElementById("hide-music").classList.add("hide-box");
+	$("#right-box").removeClass("right-box").addClass("hide-box");
+	$("#hide-music").removeClass("hide-box").addClass("right-box");
 });
 
+var listMusic = $("#list-music").click(function(event) {
+	$("#right-box").removeClass("hide-box").addClass("right-box");
+	$("#hide-music").removeClass("right-box").addClass("hide-box");
+});
+
+
+});
 
 
 
